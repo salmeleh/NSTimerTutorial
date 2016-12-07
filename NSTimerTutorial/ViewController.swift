9979788764bf1ba900e6a34155e812a4e64df212
salmeleh <salmeleh@gmail.com>
2016-12-07 13:53:19 -0600

    all 3 functioning
    using refreshInterval & 2 decimal formatting

--- NSTimerTutorial/ViewController.swift
+++ NSTimerTutorial/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var countingLabel: UILabel!
+    
+    var timer = Timer()
+    var counter = 0.00
+    var refreshInterval = 0.05
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        countingLabel.text = String(counter)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func startButton(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: refreshInterval, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    
+    @IBAction func pauseButton(_ sender: Any) {
+        timer.invalidate()
+    }
 
+    
+    
+    @IBAction func clearButton(_ sender: Any) {
+        timer.invalidate()
+        counter = 0
+        countingLabel.text = String(counter)
+    }
+    
+    
+    func updateCounter() {
+        countingLabel.text = String(format: "%.2f", counter)
+        counter += refreshInterval
+    }
+    
+    func timeString(time:TimeInterval) -> String {
+        let hours = Int(time) / 3600
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String(format:"%02i:%02i:%02i", hours, minutes, seconds)
+    }
+    
 }
 
